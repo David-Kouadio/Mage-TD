48d3ddd5988feaa79de4405f1221f891f8a5d021
David Kouadio <davidboti.f.k@gmail.com>
2026-03-10 23:01:51 +0000

    Implementação do sistema de movimento e câmera

--- Assets/Prefabs/Scripts/InputManager.cs
+++ Assets/Prefabs/Scripts/InputManager.cs
@@ -3,15 +3,43 @@ using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
 {
+    private PlayerInput playerInput;
+    private PlayerInput.OnFootActions onFoot;
+
+    private PlayerMotor motor;
+    private PlayerLook look;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
+        playerInput = new PlayerInput();
+        onFoot = playerInput.OnFoot;
         
+        motor = GetComponent<PlayerMotor>();
+        look = GetComponent<PlayerLook>();
+
+        onFoot.Jump.performed += ctx => motor.Jump();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        //Falar ao playermotor para mover usando os valores das ações de movimento.
+        motor.ProcessMove(onFoot.Movement.ReadValue<Vector2>());
     }
+    void LateUpdate()
+    {
+        look.ProcessLook(onFoot.Look.ReadValue<Vector2>());
+    }
+
+    private void OnEnable()
+    {
+        onFoot.Enable();        
+    }
+
+    private void OnDisable()
+    {
+        onFoot.Disable();       
+    }
+
 }
---
+++ Assets/Prefabs/Scripts/PlayerLook.cs
@@ -0,0 +1,26 @@
+using Unity.Mathematics;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    public Camera cam;
+    private float xRotation = 0f;
+
+    public float xSensitivity = 30f;
+    public float ySensitivity = 30f;
+
+    public void ProcessLook(Vector2 input)
+    {
+        float mouseX = input.x;
+        float mouseY = input.y;
+
+        //calcular a rotação da camera olhando para cima e para baixo
+        xRotation -= (mouseY * Time.deltaTime) * ySensitivity;
+        xRotation = Mathf.Clamp(xRotation, -80f, 80f);
+
+        //aplicar para o transform da camera
+        cam.transform.localRotation = Quaternion.Euler(xRotation, 0,0);
+        //rodar a visao para esquerda e direita
+        transform.Rotate(Vector3.up * (mouseX * Time.deltaTime) * xSensitivity);
+    }
+}
