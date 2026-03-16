b5cb5c7978cb764f17baac24817367073b304370
David Kouadio <davidboti.f.k@gmail.com>
2026-03-16 23:37:29 +0000

    Implementação do sistema de IA dos inimigos
    Também deletei Assets antigos e corrigi bugs no modelo do inimigo Pukeko.

---
+++ Assets/Prefabs/Scripts/Enemy/StateMachine.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class StateMachine : MonoBehaviour
+{
+    public BaseState activeState;
+    public PatrolState patrolState;
+
+    public void Initialise()
+    {
+        patrolState = new PatrolState();
+        ChangeState(patrolState);
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(activeState != null)
+        {
+            activeState.Perform();
+        }
+    }
+
+    public void ChangeState(BaseState newState)
+    {
+        //checar se activeState != null
+        if (activeState != null)
+        {
+            //fazer limpeza no activeState
+            activeState.Exit();
+        }
+        //mudar para novo estado
+        activeState = newState;
+
+        //checar novamente se não é null para evitar erros
+        if(activeState != null)
+        {
+            //implementar o novo estado
+            activeState.stateMachine = this;
+            activeState.enemy = GetComponent<Enemy>();
+            activeState.Enter();
+        }
+    }
+}
