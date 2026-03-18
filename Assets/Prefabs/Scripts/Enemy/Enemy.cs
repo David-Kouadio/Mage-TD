95f597eeba6f4c555b18d683763873f898535cd1
David Kouadio <davidboti.f.k@gmail.com>
2026-03-18 06:46:31 +0000

    Implementação do sistema de perseguição ao player

--- Assets/Prefabs/Scripts/Enemy/Enemy.cs
+++ Assets/Prefabs/Scripts/Enemy/Enemy.cs
@@ -10,17 +10,50 @@ public class Enemy : MonoBehaviour
     [SerializeField]
     private string currentState;
     public Path path;
+    private GameObject player;
+    public float sightDistance = 20f;
+    public float fieldOfView = 85f;
+    public float eyeHeight;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         stateMachine = GetComponent<StateMachine>();
         agent = GetComponent<NavMeshAgent>();
         stateMachine.Initialise();
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CanSeePlayer();
+    }
+    public bool CanSeePlayer()
+    {
+        if(player != null)
+        {
+            //o jogador está perto o suficiente para ser visto?
+            if(Vector3.Distance(transform.position, player.transform.position) < sightDistance)
+            {
+                Vector3 targetDirection = player.transform.position - transform.position - (Vector3.up * eyeHeight);
+                float angleToPlayer = Vector3.Angle(targetDirection, transform.forward);
+                if(angleToPlayer >= -fieldOfView && angleToPlayer <= fieldOfView)
+                {
+                    Ray ray = new Ray(transform.position + (Vector3.up * eyeHeight), targetDirection);
+                    RaycastHit hitInfo = new RaycastHit();
+                    if(Physics.Raycast(ray, out hitInfo, sightDistance))
+                    {
+                        if(hitInfo.transform.gameObject == player)
+                        {
+                            Debug.DrawRay(ray.origin, ray.direction * sightDistance);
+                            return true;
+                        }
+                    }
+                    
+                }
+
+            }
+        }
+        return false;
     }
 }
